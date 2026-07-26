50997bbb08dfb277534ca4e1ca4c7493149cf2e1
Osvaldo <osvaldodasilvasousa@gmail.com>
2026-07-26 12:02:38 -0300

    Construção da primeira tela do app(titulo e botão gerar)

--- app/src/main/java/com/example/expressoesnumericas/MainActivity.kt
+++ app/src/main/java/com/example/expressoesnumericas/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.expressoesnumericas.ui.screens.HomeScreen
-import com.example.expressoesnumericas.ui.theme.ExpressoesNumericasTheme
+import ui.theme.ExpressoesNumericasTheme
 
 class MainActivity : ComponentActivity() {
 
--- app/src/main/java/ui/theme/Color.kt
+++ app/src/main/java/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.expressoesnumericas.ui.theme
+package ui.theme
 
 import androidx.compose.ui.graphics.Color
 
