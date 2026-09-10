39caa17b8689b98c9d2718ce7bcf2c970db8ffb6
Osvaldo <osvaldodasilvasousa@gmail.com>
2026-09-09 22:26:17 -0300

    chore: increment versionCode to 3 and versionName to 1.0.0 for production release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.jumirandapisousa.nohetica.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.9.0"
+        versionCode = 3
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
